386cec16d9c6e45be1597619ed24f63a678787db
LunaLiu4 <luna.liu829@gmail.com>
2024-03-07 14:45:20 -0600

    renew the game

--- script.js
+++ script.js
@@ -3,6 +3,8 @@
 //select elements
 const score0El = document.querySelector("#score--0");
 const score1El = document.getElementById("score--1");
+console.log(score0El.textContent, score1El.textContent);
+
 const diceEl = document.querySelector(".dice");
 const btnNew = document.querySelector(".btn--new");
 const btnRoll = document.querySelector(".btn--roll");
@@ -13,16 +15,28 @@ const current1El = document.getElementById("current--1");
 const player0El = document.querySelector(".player--0");
 const player1El = document.querySelector(".player--1");
 
-//staring conditions
-score0El.textContent = 0;
-// score1El.innerHTML = 0;
-score1El.textContent = 0;
-diceEl.classList.add("hidden");
+let scores, currentScore, activePlayer, playing;
 
-let currentScore = 0;
-let activePlayer = 0;
-const scores = [0, 0];
-let playing = true;
+const init = function () {
+  currentScore = 0;
+  activePlayer = 0;
+  scores = [0, 0];
+  playing = true;
+
+  //staring conditions
+  score0El.textContent = 0;
+  // score1El.innerHTML = 0;
+  score1El.textContent = 0;
+  current0El.textContent = 0;
+  current1El.textContent = 0;
+
+  diceEl.classList.add("hidden");
+  player0El.classList.remove("player--winner");
+  player1El.classList.remove("player--winner");
+  player0El.classList.add("player--active");
+  player1El.classList.remove("player--active");
+};
+init();
 
 const switchPlayer = function () {
   // reset the score of preview player to 0
@@ -83,3 +97,5 @@ btnHold.addEventListener("click", function () {
     }
   }
 });
+
+btnNew.addEventListener("click", init); //important
